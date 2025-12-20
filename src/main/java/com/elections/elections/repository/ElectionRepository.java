5bdf0530a743242bca86a1cd224c13fe210cf43e
Norbert Piecka <norbert.piecka9@gmail.com>
2025-12-20 17:11:29 +0100

    Migration to yaml configuration and add cache

--- src/main/java/com/elections/elections/repository/ElectionRepository.java
+++ src/main/java/com/elections/elections/repository/ElectionRepository.java
@@ -2,6 +2,9 @@ package com.elections.elections.repository;
 
 import com.elections.elections.model.entity.Election;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -9,4 +12,7 @@ import java.util.List;
 public interface ElectionRepository extends JpaRepository<Election, Long> {
     boolean existsByName(String name);
     List<Election> findByStartDateTimeBeforeAndEndDateTimeAfter(LocalDateTime date1, LocalDateTime date2);
+    @Modifying
+    @Query("DELETE FROM Election e WHERE e.id = :id")
+    int deleteElectionById(@Param("id") Long id);
 }
--- src/main/java/com/elections/elections/repository/VoteRepository.java
+++ src/main/java/com/elections/elections/repository/VoteRepository.java
@@ -3,15 +3,20 @@ package com.elections.elections.repository;
 import com.elections.elections.model.entity.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
     boolean existsByElectorIdAndElectionId(Long electorId, Long electionId);
     long countByCandidateIdAndElectionId(Long candidateId, Long electionId);
     long countByElectionId(Long electionId);
     @Modifying
-    void deleteByCandidateId(Long candidateId);
+    @Query("DELETE FROM Vote v WHERE v.candidate.id = :candidateId")
+    void deleteByCandidateId(@Param("candidateId") Long candidateId);
     @Modifying
-    void deleteByElectionId(Long electionId);
+    @Query("DELETE FROM Vote v WHERE v.election.id = :electionId")
+    void deleteByElectionId(@Param("electionId") Long electionId);
     @Modifying
-    void deleteByElectorId(Long electorId);
+    @Query("DELETE FROM Vote v WHERE v.elector.id = :electorId")
+    void deleteByElectorId(@Param("electorId") Long electorId);
 }
